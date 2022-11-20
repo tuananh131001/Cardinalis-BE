171144a78465c96f254b0afd438d7c79af932597
WilliamA1pha <s3864077@rmit.edu.vn>
2022-11-20 14:36:54 +0700

    1. Base project with SPRING, SPRING DATA JPA,SECURITY

---
+++ src/main/java/com/example/cardinalisbe/CardinalisBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardinalisbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardinalisBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardinalisBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cardinalisbe/CardinalisBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardinalisbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardinalisBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
