6fbdbe467185cb3421c3220bcf7999d48c88db38
WilliamA1pha <s3864077@rmit.edu.vn>
2022-12-05 14:04:15 +0700

    sucessful merge tweet-service into microservice

--- tweet-crd/src/main/java/org/cardinalis/tweetcrd/TweetCrdApplication.java
+++ tweet-crd/src/main/java/org/cardinalis/tweetcrd/TweetCrdApplication.java
@@ -2,8 +2,10 @@ package org.cardinalis.tweetcrd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TweetCrdApplication {
 
 	public static void main(String[] args) {
