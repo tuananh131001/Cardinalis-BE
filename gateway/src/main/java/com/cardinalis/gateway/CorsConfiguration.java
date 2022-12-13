0814288d243ed62160b38738cc8080bdbc5db9ba
WilliamA1pha <s3864077@rmit.edu.vn>
2022-12-13 14:47:11 +0700

    sua cors

---
+++ gateway/src/main/java/com/cardinalis/gateway/CorsConfiguration.java
@@ -0,0 +1,29 @@
+package com.cardinalis.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class CorsConfiguration extends
+        org.springframework.web.cors.CorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("http://localhost:9002"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST","PUT", "DELETE"));
+        corsConfig.addAllowedHeader("Content-Type");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }}
