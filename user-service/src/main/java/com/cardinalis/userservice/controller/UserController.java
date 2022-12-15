bc63bbd8e5bbe89224d79058ff0573c2d079eb6e
WilliamA1pha <s3864077@rmit.edu.vn>
2022-12-16 01:30:29 +0700

    Rework controller for user

--- user-service/src/main/java/com/cardinalis/userservice/controller/UserController.java
+++ user-service/src/main/java/com/cardinalis/userservice/controller/UserController.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,6 +19,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -35,35 +39,60 @@ public class UserController {
     private ModelMapper mapper;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseDTO> createUser(@RequestBody RegisterDTO register) {
+    public ResponseEntity<Map<String, Object>> createUser(@RequestBody RegisterDTO register) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", new ArrayList<>());
+        response.put("success", true);
+        response.put("errors_message", null);
+
         UserEntity userCreated = userService.save(register);
-        return ResponseEntity.created(URI.create("/" + userCreated.getId()))
-                .body(ResponseDTO.builder()
-                        .data(mapper.map(userCreated, UserEntityDTO.class))
-                        .build());
+        response.put("code", HttpStatus.CREATED.value());
+        response.put("data", mapper.map(userCreated, UserEntityDTO.class));
+
+        return ResponseEntity.created(URI.create("/" + userCreated.getId())).body(response);
     }
+
     @GetMapping("/fetch/{username}")
-    public ResponseEntity<ResponseDTO> fetchByUsername(@PathVariable("username") String username) {
+    public ResponseEntity<Map<String, Object>> fetchByUsername(@PathVariable("username") String username) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", new ArrayList<>());
+        response.put("success", true);
+        response.put("errors_message", null);
+
         UserEntity user = userService.fetchByUsername(username);
-        return ResponseEntity.ok(ResponseDTO.builder()
-                .data(mapper.map(user, UserEntityDTO.class))
-                .build());
+        response.put("code", HttpStatus.OK.value());
+        response.put("data", mapper.map(user, UserEntityDTO.class));
+
+        return ResponseEntity.ok(response);
     }
+
     //    Will refactor this later
     @PostMapping("/login")
-    public ResponseEntity<TokenDTO> authenticate(@RequestBody @Valid LoginDTO form) {
-        UsernamePasswordAuthenticationToken login = form.converter();
+    public ResponseEntity<Map<String, Object>> authenticate(@RequestBody @Valid LoginDTO form) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("data", new ArrayList<>());
+        response.put("success", true);
+        response.put("errors_message", null);
 
         try {
+            UsernamePasswordAuthenticationToken login = form.converter();
             Authentication authentication = authManager.authenticate(login);
             String token = tokenService.generateToken(authentication);
 
-            return ResponseEntity.ok(new TokenDTO(token, "Bearer"));
+            response.put("code", HttpStatus.OK.value());
+            response.put("data", new TokenDTO(token, "Bearer"));
+
+            return ResponseEntity.ok(response);
         } catch (AuthenticationException e) {
             // return "Login Failed: Your user ID or password is incorrect" with 401
-            return ResponseEntity.status(401).build();
+            response.put("code", HttpStatus.UNAUTHORIZED.value());
+            response.put("success", false);
+            response.put("errors_message", "Login Failed: Your user ID or password is incorrect");
+
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
         }
     }
+
     @GetMapping
     public String test() {
         return "test";
