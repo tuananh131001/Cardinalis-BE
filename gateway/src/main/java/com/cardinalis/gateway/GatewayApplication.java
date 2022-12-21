94e03ccedb829c73748d7a90c67f66d9e17e4c91
WilliamA1pha <s3864077@rmit.edu.vn>
2022-12-22 02:07:05 +0700

    Gud old run local method

--- gateway/src/main/java/com/cardinalis/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/cardinalis/gateway/GatewayApplication.java
@@ -2,7 +2,9 @@ package com.cardinalis.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient// It acts as a eureka client
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
--- user-service/src/main/java/com/cardinalis/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/cardinalis/userservice/UserServiceApplication.java
@@ -2,7 +2,9 @@ package com.cardinalis.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient // It acts as a eureka client
 @SpringBootApplication
 public class UserServiceApplication {
 
