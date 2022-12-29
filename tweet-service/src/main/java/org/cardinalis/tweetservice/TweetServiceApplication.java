d90121386d2356cfbcd447b01f76b3f4d1f93505
william <s3864077@rmit.edu.vn>
2022-12-30 00:22:50 +0700

    Add spring gateway

--- tweet-service/src/main/java/org/cardinalis/tweetservice/TweetServiceApplication.java
+++ tweet-service/src/main/java/org/cardinalis/tweetservice/TweetServiceApplication.java
@@ -2,9 +2,7 @@ package org.cardinalis.tweetservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient // It acts as a eureka client
 @SpringBootApplication
 public class TweetServiceApplication {
 
--- user-service/src/main/java/com/cardinalis/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/cardinalis/userservice/UserServiceApplication.java
@@ -2,9 +2,7 @@ package com.cardinalis.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient // It acts as a eureka client
 @SpringBootApplication
 public class UserServiceApplication {
 
