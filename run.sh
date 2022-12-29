0dd53027d1d08c8aee5141c2d1fe1e487842b793
william <s3864077@rmit.edu.vn>
2022-12-30 00:42:28 +0700

    to mac

--- run.sh
+++ run.sh
@@ -5,6 +5,20 @@ set -e # stop shell when a command fails with status other than 0
 r=`pwd`
 echo $r
 
+# Gateway
+cd "$r/gateway"
+mvn clean install &
+
+# Tweet Service
+cd "$r/tweet-service"
+mvn clean install &
+
+# User Service
+cd "$r/user-service"
+mvn clean install &
+
+cd "$r"
+
 docker-compose up -d --build
 minikube addons enable ingress
 minikube addons enable ingress-dns
