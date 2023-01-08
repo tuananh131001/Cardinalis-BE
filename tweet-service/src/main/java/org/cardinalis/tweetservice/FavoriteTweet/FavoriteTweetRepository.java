464a92df0dac80b86beadf916b3725d909bf9669
hakhanhne <s3877707@rmit.edu.vn>
2023-01-09 00:59:23 +0700

    update path mapping - merge ik

--- tweet-service/src/main/java/org/cardinalis/tweetservice/FavoriteTweet/FavoriteTweetRepository.java
+++ tweet-service/src/main/java/org/cardinalis/tweetservice/FavoriteTweet/FavoriteTweetRepository.java
@@ -4,10 +4,12 @@ import org.cardinalis.tweetservice.FavoriteTweet.FavoriteTweet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FavoriteTweetRepository extends JpaRepository<FavoriteTweet, Long> {
+    List<FavoriteTweet> findByTweet_Id(Long tweetId);
 
     Optional<FavoriteTweet> findByUsernameAndTweet_Id(String username, Long tweetId);
 
