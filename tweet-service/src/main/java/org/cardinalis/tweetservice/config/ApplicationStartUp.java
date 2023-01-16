e07a8bcd977c6bcd293a2e2dad9ba9a2ef7d0137
Tuan Anh <s3864077@rmit.edu.vn>
2023-01-16 20:12:31 +0700

    delete dummy tweet

--- tweet-service/src/main/java/org/cardinalis/tweetservice/config/ApplicationStartUp.java
+++ tweet-service/src/main/java/org/cardinalis/tweetservice/config/ApplicationStartUp.java
@@ -27,49 +27,9 @@ public class ApplicationStartUp {
                 Tweet tweet1 = tweetRepository.save(saveTweet1());
                 Tweet tweet2 = tweetRepository.save(saveTweet2());
                 Tweet tweet3 = tweetRepository.save(saveTweet3());
-                Tweet tweet4 = tweetRepository.save(saveTweet4());
-                Tweet tweet5 = tweetRepository.save(saveTweet5());
                 tweetRepository.save(tweet1);
                 tweetRepository.save(tweet2);
                 tweetRepository.save(tweet3);
-                tweetRepository.save(tweet4);
-                tweetRepository.save(tweet5);
-
-                favoriteTweetRepository.save(FavoriteTweet.builder().tweet(tweet1).email("thanhnn2@gmail.com").build());
-                favoriteTweetRepository.save(FavoriteTweet.builder().tweet(tweet4).email("jjeanjacques10@github.com").build());
-                favoriteTweetRepository.save(FavoriteTweet.builder().tweet(tweet3).email("thanhnn2@gmail.com").build());
-                favoriteTweetRepository.save(FavoriteTweet.builder().tweet(tweet2).email("thanhnn2@gmail.com").build());
-                favoriteTweetRepository.save(FavoriteTweet.builder().tweet(tweet2).email("user3@gmail.com").build());
-                favoriteTweetRepository.save(FavoriteTweet.builder().tweet(tweet1).email("user3@gmail.com").build());
-                favoriteTweetRepository.save(FavoriteTweet.builder().tweet(tweet4).email("user3@gmail.com").build());
-                favoriteTweetRepository.save(FavoriteTweet.builder().tweet(tweet5).email("thanhnn2@gmail.com").build());
-                favoriteTweetRepository.save(FavoriteTweet.builder().tweet(tweet5).email("jjeanjacques10@github.com").build());
-
-                Comment commnent1 = commentRepository.save(Comment.builder().tweet(tweet1).createdAt(LocalDateTime.now()).lastEdit(LocalDateTime.now()).email("thanhnn2@gmail.com").content("tuyet").build());
-                Comment commnent2 = commentRepository.save(Comment.builder().tweet(tweet4).email("jjeanjacques10@github.com").content("ok").build());
-                Comment commnent3 = commentRepository.save(Comment.builder().tweet(tweet3).email("thanhnn2@gmail.com").content("haha vui ghe").build());
-                Comment commnent4 = commentRepository.save(Comment.builder().tweet(tweet2).email("thanhnn2@gmail.com").content("an tet nao").build());
-                Comment commnent5 = commentRepository.save(Comment.builder().tweet(tweet2).email("user3@gmail.com").content("bun ngu").build());
-                Comment commnent6 = commentRepository.save(Comment.builder().tweet(tweet1).email("user3@gmail.com").content("bun").build());
-                Comment commnent7 = commentRepository.save(Comment.builder().tweet(tweet4).email("user3@gmail.com").content("mong duoc hd").build());
-                Comment commnent8 = commentRepository.save(Comment.builder().tweet(tweet5).email("thanhnn2@gmail.com").content("haha").build());
-                Comment commnent9 = commentRepository.save(Comment.builder().tweet(tweet5).email("jjeanjacques10@github.com").content("hihi").build());
-
-                commentRepository.save(commnent1);
-                commentRepository.save(commnent2);
-                commentRepository.save(commnent3);
-                commentRepository.save(commnent4);
-                commentRepository.save(commnent5);
-                commentRepository.save(commnent6);
-                commentRepository.save(commnent7);
-                commentRepository.save(commnent8);
-                commentRepository.save(commnent9);
-
-                replyRepository.save(Reply.builder().comment(commnent2).email("thanhnn2@gmail.com").content("tuyet").build());
-                replyRepository.save(Reply.builder().comment(commnent1).email("jjeanjacques10@github.com").content("ok").build());
-                replyRepository.save(Reply.builder().comment(commnent8).email("user3@gmail.com").content("zui z").build());
-                replyRepository.save(Reply.builder().comment(commnent3).email("jjeanjacques10@github.com").content("ok").build());
-                replyRepository.save(Reply.builder().comment(commnent9).email("thanhnn2@gmail.com").content("mac gi cuoi").build());
 
 
             }
